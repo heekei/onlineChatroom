ae16fd23e1e0d8dd7367e938041f052b52afd3bf
heekei <heekei@foxmail.com>
2017-11-05 14:16:44 +0800

    fomatted

--- server/wsServer.js
+++ server/wsServer.js
@@ -21,9 +21,9 @@ chatHistory = JSON.parse(hisJSON);
 wss.on('connection', function (ws) {
     cltCollection.push(ws);
     //初始化历史记录
-    broadcast(JSON.stringify({
-        history: chatHistory
-    }));
+    broadcast({ history: chatHistory });
+
+    /* 收到消息时 */
     ws.on('message', function (message) {
         message = JSON.parse(message);
         chatHistory.push(message);
@@ -32,19 +32,16 @@ wss.on('connection', function (ws) {
         message = JSON.stringify(message);
         sendExptSelf(ws, message);
     });
+
+    /* 关闭连接时 */
     ws.on("close", function (e) {
         cltCollection.splice(cltCollection.indexOf(ws), 1);
-        broadcast(
-            JSON.stringify(
-                {
-                    "onlinecount": wss.clients.length
-                }
-            )
-        );
+        broadcast({ "onlinecount": wss.clients.length });
         fs.writeFile('history.json', JSON.stringify(chatHistory), function (err) {
             console.error(err);
         });
     })
+
     ws.send(
         JSON.stringify(
             {
@@ -53,23 +50,33 @@ wss.on('connection', function (ws) {
             }
         )
     );
-    sendExptSelf(ws,
-        JSON.stringify(
-            {
-                "onlinecount": wss.clients.length
-            }
-        )
-    )
+    
+    sendExptSelf(ws, { "onlinecount": wss.clients.length });
     console.log('当前在线人数：' + wss.clients.length)
 });
+
+/**
+ * 广播
+ * 
+ * @param {{}} data 数据
+ */
 function broadcast(data) {
+    var data = JSON.stringify(data);
     wss.clients.forEach(function (client) {
         client.send(data);
     })
 }
+
+/**
+ * 向其他客户端发送信息
+ * 
+ * @param {WebSocket} cli 发送方
+ * @param {{}} data 
+ */
 function sendExptSelf(cli, data) {
+    var data = JSON.stringify(data);
     cltCollection.forEach(function (client) {
         if (cli == client) return;
         client.send(data);
-    })
+    });
 }
